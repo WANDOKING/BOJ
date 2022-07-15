d678059b0f2c795ededb58c1d8708f4ded9c6b45
Dowan Kim <108464645+WANDOKING@users.noreply.github.com>
2022-07-15 17:49:58 +0900

    Create 10950 A+B - 3.cpp

---
+++ level03 반복문/10950 A+B - 3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    
+    cin >> T;
+    
+    for (int i = 0; i < T; i++) {
+        int A;
+        int B;
+        
+        cin >> A >> B;
+        
+        cout << A + B << endl;
+    }
+
+    return 0;
+}
