b926ff2dba7e6c286ad6b6c52c1a09fc402d8b36
Dowan Kim <108464645+WANDOKING@users.noreply.github.com>
2022-07-18 19:03:34 +0900

    Create 2742 기찍 N.cpp

---
+++ level03 반복문/2742 기찍 N.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    
+    cin >> N;
+    
+    for (int i = N; i >= 1; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
