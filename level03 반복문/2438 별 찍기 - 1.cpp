c8e119e3dc69520a05a12c7397db4589485b3010
Dowan Kim <108464645+WANDOKING@users.noreply.github.com>
2022-07-20 19:22:35 +0900

    Create 2438 별 찍기 - 1.cpp

---
+++ level03 반복문/2438 별 찍기 - 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    
+    scanf("%d", &N);
+    
+    for (int i = 1; i <= N; i++) {
+        for(int j = 1; j <= i; j++) {
+            putchar('*');
+        }
+        putchar('\n');
+    }
+
+    return 0;
+}
