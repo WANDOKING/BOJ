9832200e3f5bc7d414281faea9695c98bf63564f
Dowan Kim <108464645+WANDOKING@users.noreply.github.com>
2022-09-01 12:51:13 +0900

    Create 15651 N과 M (3).cpp

---
+++ level15 백트래킹/15651 N과 M (3).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int nums[8] = { 0, };
+
+void DfsRecursive(int depth, int N, int M)
+{
+    if (depth == M)
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            nums[depth] = i;
+            for (int j = 1; j <= M; j++)
+            {
+                printf("%d ", nums[j]);
+            }
+            printf("\n");
+        }
+        
+        return;
+    }
+    
+    for (int i = 1; i <= N; i++)
+    {
+        nums[depth] = i;
+        DfsRecursive(depth + 1, N, M);
+    }
+}
+
+int main()
+{
+    int N;
+    int M;
+    
+    scanf("%d %d", &N, &M);
+    
+    DfsRecursive(1, N, M);
+
+    return 0;
+}
