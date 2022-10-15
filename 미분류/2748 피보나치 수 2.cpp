ea4a0143d501fe249d50fd5da7c89aad1d7beda3
Dowan Kim <108464645+WANDOKING@users.noreply.github.com>
2022-10-15 23:24:21 +0900

    Create 2748 피보나치 수 2.cpp

---
+++ 미분류/2748 피보나치 수 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(void)
+{
+    int n;
+
+    long long dp[91];
+
+    scanf("%d", &n);
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    printf("%lld", dp[n]);
+}
